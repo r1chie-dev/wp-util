06684dffbc376d8a53614ddf72e3aedd0de86927
Ricardo Fernandes <ricardo.fernandes@arealmedia.com>
2024-12-11 12:27:04 +0000

    main -  new pages

--- assets/scripts/components/pages.js
+++ assets/scripts/components/pages.js
@@ -2,16 +2,18 @@ class Pages {
       
   options = {
     Admin: [
-      { name: "Admin",            redirect: "wp-admin/",                                    value: "admin" },
-      { name: "Custom Fields",    redirect: "wp-admin/admin.php?page=wapf-field-groups",    value: "customFields" },
-      { name: "New Custom Field", redirect: "wp-admin/post-new.php?post_type=wapf_product", value: "newCustomField" },
-      { name: "Products",         redirect: "wp-admin/edit.php?post_type=product",          value: "products" },
-      { name: "New Product",      redirect: "wp-admin/post-new.php?post_type=product",      value: "newProduct" },
-      { name: "Edit Product",     redirect: "wp-admin/post.php?post={0}&action=edit",       value: "editProduct" },
-      { name: "Coupons",          redirect: "wp-admin/edit.php?post_type=shop_coupon",      value: "coupons" },
-      { name: "New Coupon",       redirect: "wp-admin/post-new.php?post_type=shop_coupon",  value: "newCoupon" },
-      { name: "Orders",           redirect: "wp-admin/edit.php?post_type=shop_order",       value: "orders" },
-      { name: "See Order",        redirect: "wp-admin/post.php?post={0}&action=edit",       value: "editOrder" }
+      { name: "Admin",            redirect: "wp-admin/",                                                                        value: "admin" },
+      { name: "Custom Fields",    redirect: "wp-admin/admin.php?page=wapf-field-groups",                                        value: "customFields" },
+      { name: "New Custom Field", redirect: "wp-admin/post-new.php?post_type=wapf_product",                                     value: "newCustomField" },
+      { name: "Extra Pages",      redirect: "wp-admin/admin.php?page=wapf-field-groups&extra_pages=true",                       value: "extraPages" },
+      { name: "Box Extra Pages",  redirect: "wp-admin/admin.php?page=wapf-field-groups&extra_pages=true&extra_pages_type=box",  value: "boxExtraPages" },
+      { name: "Products",         redirect: "wp-admin/edit.php?post_type=product",                                              value: "products" },
+      { name: "New Product",      redirect: "wp-admin/post-new.php?post_type=product",                                          value: "newProduct" },
+      { name: "Edit Product",     redirect: "wp-admin/post.php?post={0}&action=edit",                                           value: "editProduct" },
+      { name: "Coupons",          redirect: "wp-admin/edit.php?post_type=shop_coupon",                                          value: "coupons" },
+      { name: "New Coupon",       redirect: "wp-admin/post-new.php?post_type=shop_coupon",                                      value: "newCoupon" },
+      { name: "Orders",           redirect: "wp-admin/edit.php?post_type=shop_order",                                           value: "orders" },
+      { name: "See Order",        redirect: "wp-admin/post.php?post={0}&action=edit",                                           value: "editOrder" }
     ],
     Frontend: [
       { name: "Home",         redirect: "",                       value:"home" },
